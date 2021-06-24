c40480f4ef394f741af668f48bd4f28d81a388d5
Mark <rmappin@ucl.ac.uk>
2021-06-24 18:34:44 +0100

    Dataloader overhauled, unpaired loader still needs work, unit tests still needed for generator and normalisation

--- utils/DataLoader.py
+++ utils/DataLoader.py
@@ -13,131 +13,266 @@ from abc import ABC, abstractmethod
 """ ImgLoader class: data_generator method for use with tf.data.Dataset.from_generator """
 
 class BaseImgLoader(ABC):
-    def __init__(self, config, dataset_type, fold):
+    def __init__(self, config: dict, dataset_type: str):
+        # Expects at least two sub-folders within data folder e.g. "AC", "VC, "HQ"
         file_path = config["DATA_PATH"]
-        self.target_path = f"{file_path}{config['DATA']['LABELS']}/"
-        self.source_path = f"{file_path}{config['DATA']['INPUTS']}/"
-        self.seg_path = f"{file_path}{config['DATA']['SEGS']}/"
-        self.ACE_list = None
-        self.NCE_list = None
-        self.seg_list = None
-        self.dataset_type = dataset_type
-        self.down_sample = config["DOWN_SAMP"]
-        self.expt_type = config["MODEL"]
-        self.norm_type = config["NORM_TYPE"]
-        self.labels = config['DATA']['LABELS'].strip('r')
-        self.json = json.load(open(f"{file_path}{config['DATA']['JSON']}", 'r'))
+        sub_folders = [f for f in os.listdir(file_path) if os.path.isdir(f"{file_path}/{f}")]
 
-        ACE_list = os.listdir(self.target_path)
-        NCE_list = os.listdir(self.source_path)
-        seg_list = os.listdir(self.seg_path)
+        if len(sub_folders) == 0:
+            raise FileNotFoundError("No subfolders found")
+
+        self._data_paths = {key: f"{file_path}/{key}" for key in sub_folders}
+        self._dataset_type = dataset_type
+        self.config = config
+        self.down_sample = config["DOWN_SAMP"]
+
+        if config["DATA"]["JSON"] != "":
+            self._json = json.load(open(f"{file_path}/{config['DATA']['JSON']}", 'r'))
+
+        # Expects list of targets and sources e.g. ["AC", "VC"], ["HQ"], leave blank for unpaired
+        self._targets = []
+        self._sources = []
+
+        if len(config["DATA"]["TARGET"]) > 0:
+            for key in config["DATA"]["TARGET"]:
+                self._targets += os.listdir(self._data_paths[key])
+
+        elif len(config["DATA"]["TARGET"]) == 0:
+            for key in sub_folders:
+                self._targets += os.listdir(self._data_paths[key])
+
+        if len(config["DATA"]["SOURCE"]) > 0:
+            for key in config["DATA"]["SOURCE"]:
+                self._sources += os.listdir(self._data_paths[key])
+
+        elif len(config["DATA"]["SOURCE"]) == 0:
+            for key in sub_folders:
+                self._sources += os.listdir(self._data_paths[key])
+
+        if len(self._targets) == 0 or len(self._sources) == 0:
+            raise FileNotFoundError(f"No data found: {len(self._targets)} targets, {len(self._sources)} sources")
 
         print("==================================================")
-        print(f"Targets: {len(ACE_list)}, sources: {len(NCE_list)}, segmentations: {len(seg_list)}")
+        print(f"Data: {len(self._targets)} targets, {len(self._sources)} sources")
 
-        unique_ids = []
+        # Get unique subject IDs for subject-level train/val split
+        self._unique_ids = []
 
-        for img_id in ACE_list:
-            if img_id[0:4] not in unique_ids:
-                unique_ids.append(img_id[0:4])
+        for img_id in self._targets:
+            if img_id[0:4] not in self._unique_ids:
+                self._unique_ids.append(img_id[0:4])
 
-        N = len(unique_ids)
-        # TODO: method to return example images
+        self._unique_ids.sort()
+        self._subject_imgs = {}
 
-        if config["CV_FOLDS"] > 0:
-            np.random.seed(5)
+        # Need procedure IDs (as poss. >1 per subject) to build ordered index of subjects' images
+        self._subject_imgs = {}
 
-            np.random.shuffle(unique_ids)
-            num_in_fold = N // config["CV_FOLDS"]
+        for img_id in self._targets + self._sources:
+            if img_id[0:6] not in self._subject_imgs.keys():
+                self._subject_imgs[img_id[0:6]] = []
+            if img_id[:-8] not in self._subject_imgs[img_id[0:6]]:
+                self._subject_imgs[img_id[0:6]].append(img_id[:-8])
 
-            if self.dataset_type == "training":
-                fold_ids = unique_ids[0:fold * num_in_fold] + unique_ids[(fold + 1) * num_in_fold:]
-            elif self.dataset_type == "validation":
-                fold_ids = unique_ids[fold * num_in_fold:(fold + 1) * num_in_fold]
+        for key in self._subject_imgs.keys():
+            self._subject_imgs[key] = sorted(self._subject_imgs[key], key=lambda x: int(x[-3:]))
+
+    def example_images(self):
+        raise NotImplementedError
+    
+    def train_val_split(self, seed: int = 5) -> None:
+        if self.config["FOLD"] > self.config["CV_FOLDS"] - 1:
+            raise ValueError(f"Fold number {self.config['FOLD']} of {self.config['CV_FOLDS']} folds")
+
+        np.random.seed(seed)
+        N = len(self._unique_ids)
+        np.random.shuffle(self._unique_ids)
+
+        # Split into folds by subject
+        if self.config["CV_FOLDS"] > 1:
+            if seed == None:
+                self._unique_ids.sort()
+
+            num_in_fold = N // self.config["CV_FOLDS"]
+
+            if self._dataset_type == "training":
+                fold_ids = self._unique_ids[0:self.config["FOLD"] * num_in_fold] + self._unique_ids[(self.config["FOLD"] + 1) * num_in_fold:]
+            elif self._dataset_type == "validation":
+                fold_ids = self._unique_ids[self.config["FOLD"] * num_in_fold:(self.config["FOLD"] + 1) * num_in_fold]
             else:
                 raise ValueError("Select 'training' or 'validation'")
 
-            self.ACE_list = [img_id for img_id in ACE_list if img_id[0:4] in fold_ids]
-            self.NCE_list = [img_id for img_id in NCE_list if img_id[0:4] in fold_ids]
-            self.seg_list = [img_id for img_id in seg_list if img_id[0:4] in fold_ids]
+            self._fold_targets = []
+            self._fold_sources = []
+            self._fold_targets = [img for img in self._targets if img[0:4] in fold_ids]
+            self._fold_sources = [img for img in self._sources if img[0:4] in fold_ids]
 
             np.random.seed()
         
-        elif config["CV_FOLDS"] == 0:
-            self.ACE_list = ACE_list
-            self.NCE_list = NCE_list
-            self.seg_list = seg_list
+        elif self.config["CV_FOLDS"] == 1:
+            self._fold_targets = self._targets
+            self._fold_sources = self._sources
         
         else:
-            raise ValueError("Number of folds must be >= 0")
+            raise ValueError("Number of folds must be > 0")
 
-        assert len(self.ACE_list) == len(self.seg_list), f"{self.ACE_list} {len(self.NCE_list)} {len(self.seg_list)}"
+        assert len(self._fold_targets) == len(self._fold_sources)
+        
+        print(f"{len(self._fold_targets)} of {len(self._targets)} examples in {self._dataset_type} folds")
+
+    @property
+    def unique_ids(self) -> list:
+        return self._unique_ids
+    
+    @property
+    def data(self) -> dict:
+        """ Return list of all images """
+        return {"targets": self._targets, "sources": self._sources}
+    
+    @property
+    def fold_data(self) -> dict:
+        """ Return list of all images in training or validation fold """
+        return {"targets": self._fold_targets, "sources": self._fold_sources}
+    
+    @property
+    def subject_imgs(self):
+        raise NotImplementedError
+    
+    def set_normalisation(self, norm_type, param_1=None, param_2=None):
+        # Mean -281.528, std = 261.552
+        if param_1 != None and param_2 != None:
+            self.param_1 = param_1
+            self.param_2 = param_2
+
+        else:
+            self.norm_type = norm_type
+
+            # If mean and std of data not available, we get rolling averages
+            if norm_type == "meanstd" or norm_type == "std":
+                mean = 0
+                std = 0
+
+                for img in self._targets + self._sources:
+                    im = np.load(f"{self._data_paths[img[6:8]]}/{img}")
+                    mean = 0.99 * mean + 0.01 * im.mean()
+                    std = 0.99 * std + 0.01 * im.std()
+                
+                self.param_1 = mean
+                self.param_2 = std
+
+            # If min and max not available, we get min and max of whole dataset
+            elif norm_type == "minmax":
+                min_val = 2048
+                max_val = -2048
+
+                for img in self._targets + self._sources:
+                    im = np.load(f"{self._data_paths[img[6:8]]}/{img}")
+                    min_val = np.min([min_val, im.min()])
+                    max_val = np.max([max_val, im.max()])
+                
+                self.param_1 = min_val
+                self.param_2 = max_val
+        
+            else:
+                raise ValueError("Choose meanstd or minmax")
+
+        print(f"{norm_type} normalisation: mean/min {self.param_1}, std/max {self.param_2}")
+        print("==================================================")
+
+        return self.param_1, self.param_2
+    
+    @property
+    def norm_params(self):
+        """ Return mean/std or min/max parameters """
+        return (self.param_1, self.param_2)
     
     @abstractmethod
     def img_pairer(self):
         raise NotImplementedError
+    
+    def _normalise(self, img):
+        if self.norm_type == "meanstd":
+            return (img - self.param_1) / self.param_2
+        elif self.norm_type == "std":
+            return img / self.param_2
+        else:
+            return (img - self.param_1) / (self.param_2 - self.param_1)
 
     def data_generator(self):
-        if self.dataset_type == "training":
-            np.random.shuffle(self.NCE_list)
+        if self._dataset_type == "training":
+            np.random.shuffle(self._fold_sources)
 
-        N = len(self.NCE_list)
+        N = len(self._fold_sources)
         i = 0
 
+        # Pair source and target images
         while i < N:
-            NCE_name = self.NCE_list[i]
-            NCE_name, ACE_name, seg_name = self.img_pairer(NCE_name)
-            ACE = np.load(ACE_name)
-            NCE = np.load(NCE_name)
-            seg = np.load(seg_name)
+            source_name = self._fold_sources[i]
+            names = self.img_pairer(source_name)
+            target_name = names["target"]
+            source_name = names["source"]
+            target = np.load(f"{self._data_paths[target_name[6:8]]}/{target_name}")
+            source = np.load(f"{self._data_paths[source_name[6:8]]}/{source_name}")
 
-            ACE = ACE[::self.down_sample, ::self.down_sample, :, np.newaxis]
-            NCE = NCE[::self.down_sample, ::self.down_sample, :, np.newaxis]
-            seg = seg[::self.down_sample, ::self.down_sample, :, np.newaxis]
-                
-            if self.norm_type == "-11":
-                ACE = ACE * 2 - 1
-                NCE = NCE * 2 - 1
+            target = target[::self.down_sample, ::self.down_sample, :, np.newaxis]
+            source = source[::self.down_sample, ::self.down_sample, :, np.newaxis]
+            target = self._normalise(target)
+            source = self._normalise(source)
 
-            yield (NCE, ACE, seg, np.array(self.json[seg_name[-20:-4]]) // self.down_sample)
+            # TODO: return index
+            yield (target, source)
 
             i += 1
 
 #-------------------------------------------------------------------------
 """ Data loader for one to one source-target pairings """
 
-class OneToOneLoader(BaseImgLoader):
-    def __init__(self, config, dataset_type, fold):
-        super().__init__(config, dataset_type, fold)
-        print("==================================================")
-        print(f"Using one-to-one loader for {self.dataset_type}")
+class PairedLoader(BaseImgLoader):
+    def __init__(self, config: dict, dataset_type: str):
+        super().__init__(config, dataset_type)
+        print(f"Using paired loader for {self._dataset_type}")
+        super().train_val_split()
+        self._subject_targets = {k: [img for img in v if img[6:8] in self.config["DATA"]["TARGET"]] for k, v in self._subject_imgs.items()}
+        self._subject_sources = {k: [img for img in v if img[6:8] in self.config["DATA"]["SOURCE"]] for k, v in self._subject_imgs.items()}
+    
+    @property
+    def subject_imgs(self):
+        """ Return list of images indexed by procedure """
+        return {"targets": self._subject_targets, "sources": self._subject_sources}
 
-    def img_pairer(self, NCE):
-        ACE = glob.glob(f"{self.target_path}{NCE[0:6]}{self.labels}*_{NCE[-7:]}")
-        assert len(ACE) == 1, ACE
-        ACE = ACE[0]
-        NCE = f"{self.source_path}{NCE}"
-        seg = f"{self.seg_path}{ACE[-20:-8]}M{ACE[-8:]}"
+    def img_pairer(self, source: str) -> dict:
+        # TODO: return idx
+        # Get potential target candidates matching source (where target and source specified)
+        target_candidates = self._subject_targets[source[0:6]]
+        assert len(target_candidates) > 0, source
+        target_stem = target_candidates[np.random.randint(len(target_candidates))]
+        target = f"{target_stem}_{source[-7:]}"
 
-        return NCE, ACE, seg
+        return {"target": target, "source": source}
 
 #-------------------------------------------------------------------------
-""" Data loader for many to one source-target pairings """
+""" Data loader for unpaired images """
 
-class ManyToOneLoader(BaseImgLoader):
-    def __init__(self, config, dataset_type, fold):
-        super().__init__(config, dataset_type, fold)
-        print("==================================================")
-        print(f"Using many-to-one loader for {self.dataset_type}")
+class UnpairedLoader(BaseImgLoader):
+    def __init__(self, config: dict, dataset_type: str):
+        super().__init__(config, dataset_type)
+        print(f"Using unpaired loader for {self._dataset_type}")
+        super().train_val_split()
 
-    def img_pairer(self, NCE):
-        ACE = glob.glob(f"{self.target_path}{NCE[0:6]}{self.labels}*_{NCE[-7:]}")
-        assert len(ACE) == 1, ACE
-        ACE = ACE[0]
-        NCE = f"{self.source_path}{NCE}"
-        seg = f"{self.seg_path}{ACE[-20:-8]}M{ACE[-8:]}"
+    @property
+    def subject_imgs(self):
+        """ Return list of images indexed by procedure """
+        return self._subject_imgs
 
-        return NCE, ACE, seg
+    def img_pairer(self, source: object, direction: str = None) -> dict:
+        # TODO: add forwards/backwards sampling, return idx
+        target_candidates = self._subject_imgs[source[0:6]]
+        target_candidates.remove(source[0:-8])
+        assert len(target_candidates) > 0, source
+        target_stem = target_candidates[np.random.randint(len(target_candidates))]
+        target = f"{target_stem}_{source[-7:]}"
+
+        return {"target": target, "source": source}
 
 #-------------------------------------------------------------------------
 """ DiffAug class for differentiable augmentation
