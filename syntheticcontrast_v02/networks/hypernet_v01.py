Bugs fixed, HyperNetwork now has instance norm and handles mb size > 1 (missing gradients however)

syntheticcontrast_v02/networks/hypernet_v01.py
@@ -37,7 +37,7 @@ class HyperNet_v01(tf.keras.layers.Layer):
         """ Takes layer embedding z, and returns kernel for that layer """
 
         a = tf.add(tf.matmul(z, self.Wi), self.bi)
-        a = tf.reshape(a, [self.in_dims, self.Nz])
+        a = tf.reshape(a, [num_kernels, self.in_dims, self.Nz])
         k = tf.add(tf.matmul(a, self.Wo), self.bo)
         k = tf.reshape(k, [num_kernels, self.f, self.f, self.d, self.in_dims, self.out_dims])
 
@@ -85,9 +85,9 @@ class LayerEmbedding(tf.keras.layers.Layer):
         """ Takes HyperNetwork as input and converts embedding to kernel """
     
         if t is None:
-            ks = tf.concat([tf.concat([tf.concat([h(self.z[i][j][k], num_kernels=1) for k in range(self.out_kernels)], axis=4) for j in range(self.in_kernels)], axis=3) for i in range(self.depth_kernels)], axis=2)
+            ks = tf.concat([tf.concat([tf.concat([h(self.z[i][j][k], num_kernels=1) for k in range(self.out_kernels)], axis=5) for j in range(self.in_kernels)], axis=4) for i in range(self.depth_kernels)], axis=3)
 
         else:
-            ks = tf.concat([tf.concat([tf.concat([h(tf.matmul(t, self.z[i][j][k]), num_kernels=t.shape[0]) for k in range(self.out_kernels)], axis=4) for j in range(self.in_kernels)], axis=3) for i in range(self.depth_kernels)], axis=2)
+            ks = tf.concat([tf.concat([tf.concat([h(tf.matmul(t[:, tf.newaxis], self.z[i][j][k]), num_kernels=t.shape[0]) for k in range(self.out_kernels)], axis=5) for j in range(self.in_kernels)], axis=4) for i in range(self.depth_kernels)], axis=3)
 
         return ks

syntheticcontrast_v02/networks/hypernetlayers.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+from .layers import InstanceNorm
+
 
 #-------------------------------------------------------------------------
 """ Down-sampling convolutional block for generator with HyperNet """
@@ -17,7 +19,7 @@ class HyperGANDownBlock(tf.keras.layers.Layer):
         self.strides = [1] + list(strides) + [1]
         
         if batch_norm:
-            self.bn = tf.keras.layers.BatchNormalization(name="batchnorm")
+            self.bn = InstanceNorm(name="instancenorm")
 
     def call(self, x, w, training):
         if w.shape[0] == 1:
@@ -25,13 +27,16 @@ class HyperGANDownBlock(tf.keras.layers.Layer):
         
         else:
             num_kernels = w.shape[0]
+            xs = []
 
             for i in range(num_kernels):
-                x[i, ...] = tf.nn.conv3d(x[i, ...], w[i, ...], self.strides, padding="SAME", name="conv")
+                xs.append(tf.nn.conv3d(x[i, ...][tf.newaxis, ...], w[i, ...], self.strides, padding="SAME", name="conv"))
+
+            x = tf.concat(xs, axis=0)
 
         # As we are using batchnorm, there is no bias term
         if self.batch_norm:
-            x = self.bn(x, training)
+            x = self.bn(x)
 
         return tf.nn.leaky_relu(x, alpha=0.2, name="l_relu")
 
@@ -51,11 +56,10 @@ class HyperGANUpBlock(tf.keras.layers.Layer):
         super().__init__(name=name)
         self.batch_norm = batch_norm
         self.dropout = dropout
-        self.strides = [1] + list(strides) + [1]
 
         if batch_norm:
-            self.bn1 = tf.keras.layers.BatchNormalization(name="batchnorm1")
-            self.bn2 = tf.keras.layers.BatchNormalization(name="batchnorm2")
+            self.bn1 = InstanceNorm(name="instancenorm1")
+            self.bn2 = InstanceNorm(name="instancenorm2")
 
         if dropout:
             self.dropout = tf.keras.layers.Dropout(0.5, name="dropout")
@@ -71,13 +75,16 @@ class HyperGANUpBlock(tf.keras.layers.Layer):
 
         else:
             num_kernels = w1.shape[0]
+            xs = []
 
             for i in range(num_kernels):
-                x[i, ...] = tf.nn.conv3d(x[i, ...], w1[i, ...], self.strides, padding="SAME", name="tconv")
+                xs.append(tf.nn.conv3d(x[i, ...][tf.newaxis, ...], w1[i, ...], (1, 1, 1, 1, 1), padding="SAME", name="tconv"))
 
-        # As we are using batchnorm, there is no bias term
+            x = tf.concat(xs, axis=0)
+
+        # As we are using instance norm, there is no bias term
         if self.batch_norm:
-            x = self.bn1(x, training=training)
+            x = self.bn1(x)
         
         if self.dropout:
             x = self.dropout(x, training=training)
@@ -90,12 +97,15 @@ class HyperGANUpBlock(tf.keras.layers.Layer):
 
         else:
             num_kernels = w2.shape[0]
+            xs = []
 
             for i in range(num_kernels):
-                x[i, ...] = tf.nn.conv3d(x[i, ...], w1[i, ...], self.strides, padding="SAME", name="conv")
+                xs.append(tf.nn.conv3d(x[i, ...][tf.newaxis, ...], w1[i, ...], (1, 1, 1, 1, 1), padding="SAME", name="conv"))
+
+            x = tf.concat(xs, axis=0)
 
         if self.batch_norm:
-            x = self.bn2(x, training=training)
+            x = self.bn2(x)
 
         if self.dropout:
             x = self.dropout(x, training=training)

syntheticcontrast_v02/networks/layers.py
@@ -10,7 +10,7 @@ class InstanceNorm(tf.keras.layers.Layer):
         super().__init__(name=name)
         self.episilon = 1e-12
 
-    def call(self, x):
+    def call(self, x, training=None):
         mu = tf.math.reduce_mean(x, axis=[1, 2, 3], keepdims=True)
         sigma = tf.math.reduce_std(x, axis=[1, 2, 3], keepdims=True)
 
@@ -95,26 +95,33 @@ class GANDownBlock(tf.keras.layers.Layer):
         - initialiser: e.g. keras.initializers.RandomNormal
         - batch_norm: True/False """
 
-    def __init__(self, nc, weights, strides, initialiser, batch_norm=True, name=None):
+    def __init__(self, nc, weights, strides, initialiser, model, batch_norm=True, name=None):
         super().__init__(name=name)
         self.batch_norm = batch_norm
         bias = not batch_norm
 
         self.conv = tf.keras.layers.Conv3D(nc, weights, strides=strides, padding="same", kernel_initializer=initialiser, use_bias=bias, name="conv")
 
-        # Instance normalisation
-        if batch_norm:
+        # Normalisation
+        if batch_norm and model == "generator":
             self.bn = InstanceNorm(name="instancenorm")
+        elif batch_norm and model == "discriminator":
+            self.bn = tf.keras.layers.BatchNormalization(name="batchnorm")
+
+        if model == "generator":
+            self.time_reps = 1
+        else:
+            self.time_reps = 2
 
     def call(self, x, t=None, training=True):
         if t is not None:
-            tiled_time = tf.tile(tf.reshape(t, [-1, 1, 1, 1, 1]), [1] + x.shape[1:4] + [1], "time_tile")
+            tiled_time = tf.tile(tf.reshape(t, [-1, 1, 1, 1, 1]), [self.time_reps] + x.shape[1:4] + [1], "time_tile")
             x = tf.concat([x, tiled_time], axis=4, name="time_concat")
 
         x = self.conv(x)
 
         if self.batch_norm:
-            x = self.bn(x)
+            x = self.bn(x, training=training)
 
         return tf.nn.leaky_relu(x, alpha=0.2, name="l_relu")
 

syntheticcontrast_v02/networks/models.py
@@ -81,6 +81,7 @@ class Discriminator(tf.keras.Model):
                         kernel,
                         strides,
                         initialiser=initialiser,
+                        model="discriminator",
                         batch_norm=batch_norm, name=f"down_{i}"))
             
             self.conv_list.append(tf.keras.layers.Conv3D(
@@ -88,12 +89,17 @@ class Discriminator(tf.keras.Model):
                 padding='valid',
                 kernel_initializer=initialiser, name="output"))
 
-    def build_model(self, x):
+        layer_names = [layer.name for layer in self.conv_list]
+
+        for time_input in self.time_layers:
+            assert time_input in layer_names, (time_input, layer_names)
+
+    def build_model(self, x, t=None):
 
         """ Build method takes tf.zeros((input_dims)) and returns
             shape of output - all layers implicitly built and weights set to trainable """
 
-        return self(x).shape
+        return self(x, t).shape
 
     def call(self, x, t=None):
 
@@ -160,6 +166,7 @@ class Generator(tf.keras.Model):
                     kernel,
                     strides,
                     initialiser=initialiser,
+                    model="generator",
                     batch_norm=True, name=f"down_{i}"))
 
         self.bottom_layer = GANDownBlock(
@@ -167,6 +174,7 @@ class Generator(tf.keras.Model):
             kernel,
             strides,
             initialiser=initialiser,
+            model="generator",
             batch_norm=True, name="bottom")
 
         cache["strides"].append(strides)
@@ -219,6 +227,7 @@ class Generator(tf.keras.Model):
                 x = conv(x, t, training=True)
             else:
                 x = conv(x, training=True)
+
             skip_layers.append(x)
 
         if self.bottom_layer.name in self.time_layers:
@@ -285,7 +294,7 @@ class HyperGenerator_v01(tf.keras.Model):
         # First layer is standard non-HyperNet conv layer
         self.first_layer = GANDownBlock(
             ngf, (4, 4, 4), (2, 2, 2),
-            initialiser=initialiser, batch_norm=True, name="down_0")
+            initialiser=initialiser, model="generator", batch_norm=True, name="down_0")
 
         for i in range(1, num_layers - 1):
             channels = np.min([ngf * 2 ** i, 512])

syntheticcontrast_v02/networks/pix2pix.py
@@ -55,7 +55,7 @@ class Pix2Pix(tf.keras.Model):
 
     def discriminator_init(self, config):
         # Get discriminator patch size
-        D_input_size = [1] + self.img_dims + [2]
+        D_input_size = [2] + self.img_dims + [2]
         self.Discriminator = Discriminator(self.initialiser, config, name="discriminator")
 
         if self.input_times:
@@ -112,7 +112,7 @@ class Pix2Pix(tf.keras.Model):
         tf.keras.Model(inputs=source, outputs=outputs).summary()
 
     @tf.function
-    def train_step(self, real_source, real_target, seg=None, times=None):
+    def train_step(self, real_source, real_target, seg=None, source_times=None, target_times=None):
 
         """ Expects data in order 'source, target' or 'source, target, segmentations'"""
 
@@ -120,7 +120,7 @@ class Pix2Pix(tf.keras.Model):
 
             # Generate fake target
             if self.input_times:
-                fake_target = self.Generator(real_source, times)
+                fake_target = self.Generator(real_source, target_times)
             else:
                 fake_target = self.Generator(real_source)
             
@@ -144,7 +144,7 @@ class Pix2Pix(tf.keras.Model):
             d_in = tf.concat([real_in, fake_in], axis=0, name="d_real_fake_concat")
 
             # Generate predictions and calculate losses
-            d_pred = self.Discriminator(d_in)
+            d_pred = self.Discriminator(d_in, target_times)
             mb_size = d_pred.shape[0] // 2
             d_loss = self.d_loss(d_pred[0:mb_size, ...], d_pred[mb_size:, ...])
             g_loss = self.g_loss(d_pred[mb_size:, ...])
@@ -161,11 +161,11 @@ class Pix2Pix(tf.keras.Model):
         self.g_metric.update_state(g_loss)
 
     @tf.function
-    def test_step(self, real_source, real_target, seg=None, times=None):
+    def test_step(self, real_source, real_target, seg=None, source_times=None, target_times=None):
 
         # Generate fake target
         if self.input_times:
-            fake_target = self.Generator(real_source, times)
+            fake_target = self.Generator(real_source, target_times)
         else:
             fake_target = self.Generator(real_source)
 
@@ -222,12 +222,12 @@ class HyperPix2Pix(Pix2Pix):
         print(f"Generator: {np.sum([np.prod(v.shape) for v in self.Generator.trainable_variables])}")
         print("===========================================================")
         tf.keras.Model(inputs=source, outputs=outputs).summary()
-        source = tf.keras.Input(shape=self.img_dims + [len(self.d_in_ch)])
+        source = tf.keras.Input(shape=self.img_dims + [2])
 
         if self.input_times:
             outputs = self.Discriminator.call(source, tf.zeros(1))
         else:
-            outputs = self.Generator.call(source)    
+            outputs = self.Discriminator.call(source)    
 
         print("===========================================================")
         print(f"Discriminator: {np.sum([np.prod(v.shape) for v in self.Discriminator.trainable_variables])}")